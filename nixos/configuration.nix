0e9521c7066b43eef6dcb872a322cd4ad06556f0
dosed-dev <dev.dozed@aleeas.com>
2024-03-05 00:46:40 +0300

    Use mesa radv instead of amdvlk

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,6 +22,7 @@
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
   ];
+  hardware.amdgpu.amdvlk = false;
 
   nixpkgs = {
     # You can add overlays here
