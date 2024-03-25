7d6c0dd7e71aacc31d923aaa31cf3a943ba5d698
dosed-dev <dev.dozed@aleeas.com>
2024-03-26 00:35:28 +0300

    Add nixpkgs-unstable as pkgs.unstable

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-23.11";
@@ -26,6 +30,10 @@
   } @ inputs: let
     inherit (self) outputs;
   in {
+
+    # Your custom packages and modifications, exported as overlays
+    overlays = import ./overlays {inherit inputs;};
+
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
