9aa6d8a4463da1a0f2e91f47df2efdca92bfa26a
dosed-dev <dev.dozed@aleeas.com>
2024-05-10 00:46:41 +0300

    FIX: intel-gpu doesn't work for some reason?

--- nixos/home-desktop/configuration.nix
+++ nixos/home-desktop/configuration.nix
@@ -14,7 +14,7 @@
     inputs.nixos-hardware.nixosModules.common-pc
     inputs.nixos-hardware.nixosModules.common-pc-ssd
     inputs.nixos-hardware.nixosModules.common-cpu-intel
-    inputs.nixos-hardware.nixosModules.common-gpu-intel
+    #inputs.nixos-hardware.nixosModules.common-gpu-intel
     inputs.nixos-hardware.nixosModules.common-gpu-amd
 
     # You can also split up your configuration and import pieces of it here:
