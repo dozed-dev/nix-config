6107d4fd0d0c28295f0590c1b9d59a9320437a10
dosed-dev <dev.dozed@aleeas.com>
2024-05-10 06:07:01 +0300

    Revert "Override ddcci-driver version, the dumb way"
    ddcci-driver regression, doesn't detect displays automatically. See
    https://gitlab.com/ddcci-driver-linux/ddcci-driver-linux/-/merge_requests/15#note_1830989470
    
    This reverts commit d506028e6dcc03ef32c2ba6ac2f856caf22b2927.

--- nixos/ddcci.nix
+++ nixos/ddcci.nix
@@ -1,24 +1,6 @@
 { config, pkgs, ...}: {
   # Kernel modules
-
-  boot.extraModulePackages =
-  let
-    kernelPackages = config.boot.kernelPackages.extend (
-      final: prev: {
-        ddcci-driver = prev.ddcci-driver.overrideAttrs (oldAttrs: rec {
-          version = "3eb20df6"; # Latest master as of 2024-05-10
-          src = pkgs.fetchFromGitLab {
-            owner = "${oldAttrs.pname}-linux";
-            repo = "${oldAttrs.pname}-linux";
-            rev = version;
-            hash = "sha256-bvg/eQiCL/C/RhXZl11f5C36ZFtVgpZeM8RJY9qnw6Q=";
-          };
-        });
-      }
-    );
-  in
-    with kernelPackages; [ ddcci-driver ];
-
+  boot.extraModulePackages = with config.boot.kernelPackages; [ ddcci-driver ];
   boot.kernelModules = [ "ddcci" "i2c-dev" ];
 
   # i2c group
