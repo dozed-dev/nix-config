c090d91fac5e89c9aa400d97de5a8923a9928bd3
dosed-dev <dev.dozed@aleeas.com>
2024-06-10 14:05:33 +0300

    Add u2f to sudo and login

--- nixos/yubikey.nix
+++ nixos/yubikey.nix
@@ -2,10 +2,21 @@
 
   services.pcscd.enable = true;
   environment.systemPackages = with pkgs; [
-    yubikey-manager pcsclite
+    yubikey-manager
+    pcsclite
+    pam_u2f
   ];
 
   services.udev.packages = with pkgs; [
     yubikey-personalization
   ];
+
+  security.pam = {
+    u2f.cue = true;
+    services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
+      kde.u2fAuth = true;
+    };
+  };
 }
