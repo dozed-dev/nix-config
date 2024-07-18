37d12fe75b077f6dd6ea94322190fc33e43c87b8
dosed-dev <dev.dozed@aleeas.com>
2024-07-19 01:50:27 +0300

    Fixes

--- nixos/plasma-desktop.nix
+++ nixos/plasma-desktop.nix
@@ -20,8 +20,6 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -38,7 +36,7 @@
 
   # GUI utils
   environment.systemPackages = with pkgs; [
-    gnome.adwaita-icon-theme
+    adwaita-icon-theme
     wl-clipboard
     kdePackages.kdialog
   ];
--- nixos/yubikey.nix
+++ nixos/yubikey.nix
@@ -12,7 +12,7 @@
   ];
 
   security.pam = {
-    u2f.cue = true;
+    u2f.settings.cue = true;
     services = {
       login.u2fAuth = true;
       sudo.u2fAuth = true;
