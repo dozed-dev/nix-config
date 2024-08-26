ea089209435c0f368e62a07a70580d4cbb0e723a
dosed-dev <dev.dozed@aleeas.com>
2024-08-26 16:16:44 +0300

    Update Fjord

--- overlays/fjordlauncher.nix
+++ overlays/fjordlauncher.nix
@@ -2,13 +2,13 @@
 final: prev: rec {
   fjordlauncher-unwrapped = prev.prismlauncher-unwrapped.overrideAttrs (oldAttrs: rec {
     pname = "fjordlauncher-unwrapped";
-    version = "8.4.1";
+    version = "8.4.2";
 
     src = prev.fetchFromGitHub {
       owner = "unmojang";
       repo = "FjordLauncher";
       rev = "${version}";
-      hash = "sha256-eE4twtyfx3SfMNu8r5VxThwQm4BRLYhpTtk8rgVmyGE=";
+      hash = "sha256-y7V8ILv11CN2fm10NAKyogMI9zlzimg08DXyUKyUREs=";
     };
 
     # Disable DRM again (revert the commits that enable it)
