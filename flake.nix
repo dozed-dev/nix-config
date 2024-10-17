2055b090e9f66c5bb27aa4179736e56f82b4a7b4
dosed-dev <dev.dozed@aleeas.com>
2024-10-17 17:34:36 +0300

    Revert "Switch to unstable (again)"
    This reverts commit b57584701f669f77bc9b3718ca0d647fe22fddfc.

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
       home-desktop = let
-        nixpkgs = inputs.nixpkgs-unstable;
+        nixpkgs = inputs.nixpkgs-stable;
       in nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
@@ -74,8 +74,8 @@
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
       "quitzka@home-desktop" = let
-        nixpkgs = inputs.nixpkgs-unstable;
-        home-manager = inputs.home-manager-unstable;
+        nixpkgs = inputs.nixpkgs-stable;
+        home-manager = inputs.home-manager-stable;
       in home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
