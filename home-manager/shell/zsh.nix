6688e69ce80d1e7cb5b73ac835b31bb6259b7d34
dosed-dev <dev.dozed@aleeas.com>
2024-10-19 19:36:29 +0300

    Move zsh config from main home.nix

---
+++ home-manager/shell/zsh.nix
@@ -0,0 +1,51 @@
+{ pkgs, ... }: {
+
+  home.shellAliases = {
+    e = "$EDITOR";
+  };
+
+  programs.zsh = {
+    enable = true;
+    dotDir = ".config/zsh";
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    enableVteIntegration = true;
+    history = rec {
+      size = 2147483647;
+      save = size;
+    };
+    oh-my-zsh = {
+      enable = true;
+      theme = ""; # We are loading p10k as standalone plugin!
+    };
+
+    initExtraFirst = ''
+      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+      INSTANT_PROMPT="''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      if [[ -r "$INSTANT_PROMPT" ]]; then source "$INSTANT_PROMPT"; fi
+    '';
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./.;
+        file = "p10k.zsh";
+      }
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          hash = "sha256-Z6EYQdasvpl1P78poj9efnnLj7QQg13Me8x1Ryyw+dM=";
+        };
+      }
+    ];
+  };
+}
