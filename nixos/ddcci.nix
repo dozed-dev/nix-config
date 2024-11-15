c8fa2598c2d416b65e917817444ae4f862d980f4
dosed-dev <dev.dozed@aleeas.com>
2024-11-16 00:41:07 +0300

    DDC/CI driver fix

--- nixos/ddcci.nix
+++ nixos/ddcci.nix
@@ -8,12 +8,12 @@
     kernelPackages = config.boot.kernelPackages.extend (
       final: prev: {
         ddcci-driver = prev.ddcci-driver.overrideAttrs (oldAttrs: rec {
-          version = "0.4.5";
+          version = "0.4.6";
           src = pkgs.fetchFromGitLab {
             owner = "nullbytepl";
             repo = "${oldAttrs.pname}-linux";
-            rev = "7853cbfc28bc62e87db79f612568b25315397dd0";
-            hash = "sha256-QImfvYzMqyrRGyrS6I7ERYmteaTijd8ZRnC6+bA9OyM=";
+            rev = "e0605c9cdff7bf3fe9587434614473ba8b7e5f63";
+            hash = "sha256-ic6qmWqj7+Ga5SGgMOckDwa5ouyvpW1LlZhY1OBr9Gk=";
           };
           patches = [];
         });
