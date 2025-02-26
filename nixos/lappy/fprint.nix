e91c87b8e94d1cf7e131682e2b6af3672d227491
dozed-dev <dev.dozed@aleeas.com>
2025-02-26 08:55:54 +0300

    remove old fprintd

--- nixos/lappy/fprint.nix
+++ nixos/lappy/fprint.nix
@@ -12,26 +12,10 @@
     group = config.users.users.nobody.group;
   };
 
-  # Start the driver at boot
-  systemd.services.fprintd = {
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig.Type = "simple";
-  };
-  
   services."06cb-009a-fingerprint-sensor" = {
     enable = true;
     backend = "python-validity";
     #backend = "libfprint-tod";
     #calib-data-file = config.sops.secrets.lappy-fp-calib.path;
   };
-
-  # Install the driver
-  #services.fprintd =
-  #{
-  #  enable = true;
-  #  # If simply enabling fprintd is not enough, try enabling fprintd.tod...
-  #  tod.enable = true;
-  #  # ...and use one of the drivers
-  #  tod.driver = pkgs.libfprint-2-tod1-vfs0090; # Goodix driver module
-  #};
 }
