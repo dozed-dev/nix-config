f4d4645b33784397cf742d899bdb0bc3d9c60edd
dozed-dev <dev.dozed@aleeas.com>
2025-02-26 10:33:18 +0300

    move fprint file
    aaa

--- nixos/lappy/configuration.nix
+++ nixos/lappy/configuration.nix
@@ -12,15 +12,14 @@
   imports = [
     # If you want to use modules from other flakes (such as nixos-hardware):
     inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t480
-
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
     ./networking.nix
+    ./fprint.nix
 
-    ../fprint.nix
     ../locale.nix
     ../nixpkgs.nix
     ../plasma-desktop.nix
