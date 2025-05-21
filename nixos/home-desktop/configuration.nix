d767d21ebd72c82ffa7043a7123e0748983fdcaa
dosed-dev <dev.dozed@aleeas.com>
2025-05-21 04:07:49 +0300

    Enable intel gpu on home-desktop

--- nixos/home-desktop/configuration.nix
+++ nixos/home-desktop/configuration.nix
@@ -11,7 +11,7 @@
     inputs.nixos-hardware.nixosModules.common-pc
     inputs.nixos-hardware.nixosModules.common-pc-ssd
     inputs.nixos-hardware.nixosModules.common-cpu-intel
-    #inputs.nixos-hardware.nixosModules.common-gpu-intel
+    inputs.nixos-hardware.nixosModules.common-gpu-intel
     inputs.nixos-hardware.nixosModules.common-gpu-amd
     inputs.chaotic.nixosModules.default
     inputs.microvm.nixosModules.host
