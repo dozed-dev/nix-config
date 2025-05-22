20b6b436c6f408d90a9ab853eab91e97cbe34686
dosed-dev <dev.dozed@aleeas.com>
2025-05-23 01:42:16 +0300

    Move helix config to home-manager

--- home-manager/helix.nix
+++ home-manager/helix.nix
@@ -2,5 +2,32 @@
   programs.helix = {
     enable = true;
     defaultEditor = true;
+    # https://docs.helix-editor.com/configuration.html
+    settings = {
+      theme = "adwaita-dark";
+      editor = {
+        line-number = "relative";
+        auto-save = true;
+        cursor-shape = {
+          normal = "block";
+        };
+      };
+      keys.normal = {
+        "0" = "goto_line_start";
+        "$" = "goto_line_end";
+        G = "goto_last_line";
+
+        space.q = ":quit";
+      };
+    };
+    # https://docs.helix-editor.com/languages.html
+    languages = {
+      language-server.nixd = { command = "nixd"; };
+
+      language = [{
+        name = "nix";
+        language-servers = [ "nixd" ];
+      }];
+    };
   };
 }
