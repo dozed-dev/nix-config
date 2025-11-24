33451c88e5e9f7de5c16b6d08597c7a43007010a
dozed-dev <dev.dozed@aleeas.com>
2025-11-24 06:30:21 +0300

    switch to mainline ddcci

--- nixos/ddcci.nix
+++ nixos/ddcci.nix
@@ -8,12 +8,12 @@
     kernelPackages = config.boot.kernelPackages.extend (
       final: prev: {
         ddcci-driver = prev.ddcci-driver.overrideAttrs (oldAttrs: rec {
-          version = "0.4.6";
+          version = "0.4.6-unstable";
           src = pkgs.fetchFromGitLab {
-            owner = "nullbytepl";
-            repo = "${oldAttrs.pname}-linux";
-            rev = "e0605c9cdff7bf3fe9587434614473ba8b7e5f63";
-            hash = "sha256-ic6qmWqj7+Ga5SGgMOckDwa5ouyvpW1LlZhY1OBr9Gk=";
+            owner = "ddcci-driver-linux";
+            repo = "ddcci-driver-linux";
+            rev = "master";
+            hash = "sha256-fQjsDjbtFKhs0bUCFfKRgCg516TXdwIkhKEbIISjgs0=";
           };
           patches = [];
         });
