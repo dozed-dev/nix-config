9f976634b6c7302612443f3179af56c5f60ae55f
dozed-dev <dev.dozed@aleeas.com>
2025-12-11 16:28:13 +0300

    remove ida

--- home-manager/lappy/home.nix
+++ home-manager/lappy/home.nix
@@ -16,8 +16,8 @@
 
     saleae-logic-2
     flashprog
-    (callPackage ida-pro {
-      ida-portable = /nix/store/x09ld7bgma45m7v65vii1myr22h8ckhw-ida-pro-9.1;
-    })
+    #(callPackage ida-pro {
+    #  ida-portable = /nix/store/x09ld7bgma45m7v65vii1myr22h8ckhw-ida-pro-9.1;
+    #})
   ];
 }
