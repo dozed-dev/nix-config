3ef5ebbfe16ab7b2d8fd317aaa04db133b511d88
dozed-dev <dev.dozed@aleeas.com>
2026-04-12 13:57:04 +0300

    remove orca patches

--- overlays/default.nix
+++ overlays/default.nix
@@ -21,34 +21,6 @@
       };
     });
     LycheeSlicer-patched = prev.callPackage ./LycheeSlicer.nix { oldLycheeSlicer = prev.LycheeSlicer; };
-    orca-slicer-patched = prev.orca-slicer.overrideAttrs (oldAttrs: {
-      src = prev.fetchFromGitHub {
-        owner = "mnott";
-        repo = "OrcaSlicer";
-        rev = "6a398a03472dc4206bc1fddfbb6ce79db8e7de40";
-        sha256 = "sha256-xNedalhtLD65/EbwnxokvC70vs6lfJZVcK+t0uJnDKw=";
-      };
-      cmakeFlags =
-        let
-          filtered =
-            builtins.filter
-              (flag:
-                !(prev.lib.hasInfix "LIBNOISE_LIBRARY" flag))
-              oldAttrs.cmakeFlags;
-        in
-          filtered ++ [
-            (prev.lib.cmakeFeature "LIBNOISE_LIBRARY_RELEASE" "${prev.libnoise}/lib/libnoise-static.a")
-          ];
-      patches = oldAttrs.patches ++ [
-        ./revert-draco.patch
-        #(prev.fetchpatch {
-        #  name = "revert-draco.patch";
-        #  url = "https://github.com/SoftFever/OrcaSlicer/commit/bb30999673cdac7644f7c4529259cce236c3b813.patch";
-        #  hash = "sha256-3hedO+z4x1Cgs6HLL+XR83MJCTqLB3TniChiNPGM4Dk=";
-        #  revert = true;
-        #})
-      ];
-    });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
